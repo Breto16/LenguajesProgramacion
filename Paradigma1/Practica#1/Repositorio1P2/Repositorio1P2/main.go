63e2222d89573487d755bea42432e7e86e8907c7
Breto16 <50472204+Breto16@users.noreply.github.com>
2023-08-25 11:01:31 -0600

    ADD F3

--- Paradigma1/Practica#1/Repositorio1P2/Repositorio1P2/main.go
+++ Paradigma1/Practica#1/Repositorio1P2/Repositorio1P2/main.go
@@ -5,20 +5,30 @@ import (
 	"strings"
 )
 
-func Funcion2() {
-	/*largo := 5 //Cantidad de *
-	extension := largo + largo - 1
-	//inicial := 1
-	for inicial := 0; inicial <= extension; inicial++ {
-		if inicial%2 == 0 {
-			//par
-			fmt.Print("*")
-		} else {
-			//impar
-			fmt.Print(" ")
-		}
-	}*/
-	tamano := 3
+func Funcion1(text string) {
+	Caracteres := len(text)
+	Palabras := strings.Fields(text)
+	NPalabras := len(Palabras)
+	Lineas := strings.Split(text, "\n")
+	NLineas := len(Lineas)
+
+	fmt.Printf("Número de caracteres: %d\n", Caracteres)
+	fmt.Printf("Número de palabras: %d\n", NPalabras)
+	fmt.Printf("Número de líneas: %d\n", NLineas)
+}
+func IniciarF2() {
+	entrada := 0
+	for entrada%2 == 0 {
+		fmt.Print("Ingrese un número para la funcion 2, debe ser siempre impar: ")
+		fmt.Scan(&entrada)
+	}
+	2
+	cantidad := (entrada + 1) / 2
+	Funcion2(cantidad)
+}
+func Funcion2(entrada int) {
+
+	tamano := entrada
 
 	for i := 0; i < tamano*2-1; i++ {
 		espacios := tamano - i - 1
@@ -39,28 +49,39 @@ func Funcion2() {
 	}
 
 }
-func abs(n int) int {
-	if n < 0 {
-		return -n
+func rotar(lista []interface{}, nRotaciones int, direccion int) {
+	length := len(lista)
+
+	if length == 0 {
+		return
 	}
-	return n
-}
-func Funcion1(text string) {
-	Caracteres := len(text)
-	Palabras := strings.Fields(text)
-	NPalabras := len(Palabras)
-	Lineas := strings.Split(text, "\n")
-	NLineas := len(Lineas)
 
-	fmt.Printf("Número de caracteres: %d\n", Caracteres)
-	fmt.Printf("Número de palabras: %d\n", NPalabras)
-	fmt.Printf("Número de líneas: %d\n", NLineas)
-}
+	nRotaciones = nRotaciones % length
 
+	if direccion == 0 { // Rotación a la izquierda
+		copy(lista, append(lista[nRotaciones:], lista[:nRotaciones]...))
+	} else if direccion == 1 { // Rotación a la derecha
+		copy(lista, append(lista[length-nRotaciones:], lista[:length-nRotaciones]...))
+	}
+}
 func main() {
 
-	text := "Este es un ejemplo de texto\ncon varias líneas\npara contar."
-	Funcion1(text)
+	texto := "Este es un ejemplo de texto\ncon varias líneas\npara contar."
+	fmt.Print(texto)
+	fmt.Println()
+	Funcion1(texto)
+	IniciarF2()
 
-	Funcion2()
+	listaInicio := []interface{}{"a", "b", "c", "d", "e", "f", "g", "h"}
+	fmt.Println("Secuencia Original =", listaInicio)
+
+	nRotaciones := 4
+	direccion := 0 // 0 para izquierda, 1 para derecha
+
+	listaFinal := make([]interface{}, len(listaInicio))
+	copy(listaFinal, listaInicio)
+
+	rotar(listaFinal, nRotaciones, direccion)
+	fmt.Println("Secuencia final rotada =", listaFinal)
+
 }
