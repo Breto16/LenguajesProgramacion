5d8b862954bdf9cd222f8eb1bfc11f38b6f70a05
Breto16 <50472204+Breto16@users.noreply.github.com>
2023-08-25 11:58:49 -0600

    ADD F4

--- Paradigma1/Practica#1/Repositorio1P2/Repositorio1P2/main.go
+++ Paradigma1/Practica#1/Repositorio1P2/Repositorio1P2/main.go
@@ -50,19 +50,48 @@ func Funcion2(entrada int) {
 
 }
 func rotar(lista []interface{}, nRotaciones int, direccion int) {
-	length := len(lista)
+	largo := len(lista)
 
-	if length == 0 {
+	if largo == 0 {
 		return
 	}
 
-	nRotaciones = nRotaciones % length
+	nRotaciones = nRotaciones % largo
 
 	if direccion == 0 { // Rotación a la izquierda
 		copy(lista, append(lista[nRotaciones:], lista[:nRotaciones]...))
 	} else if direccion == 1 { // Rotación a la derecha
-		copy(lista, append(lista[length-nRotaciones:], lista[:length-nRotaciones]...))
+		copy(lista, append(lista[largo-nRotaciones:], lista[:largo-nRotaciones]...))
 	}
+}
+type calzado struct {
+	modelo string
+	precio float64
+	talla  int
+	stock  int
+}
+
+var inventario []calzado
+
+func agregarZapato(modelo string, precio float64, talla int, stock int) {
+	zapato := calzado{modelo, precio, talla, stock}
+	inventario = append(inventario, zapato)
+}
+
+func venderZapato(modelo string, talla int) {
+	for i, zapato := range inventario {
+		if zapato.modelo == modelo && zapato.talla == talla {
+			if zapato.stock > 0 {
+				inventario[i].stock--
+				fmt.Printf("Venta realizada: Zapato %s talla %d vendido\n", modelo, talla)
+				return
+			} else {
+				fmt.Printf("No hay stock disponible para el zapato %s talla %d\n", modelo, talla)
+				return
+			}
+		}
+	}
+	fmt.Printf("El zapato %s talla %d no se encuentra en el inventario\n", modelo, talla)
 }
 func main() {
 
@@ -74,14 +103,31 @@ func main() {
 
 	listaInicio := []interface{}{"a", "b", "c", "d", "e", "f", "g", "h"}
 	fmt.Println("Secuencia Original =", listaInicio)
-
 	nRotaciones := 4
 	direccion := 0 // 0 para izquierda, 1 para derecha
-
 	listaFinal := make([]interface{}, len(listaInicio))
 	copy(listaFinal, listaInicio)
-
 	rotar(listaFinal, nRotaciones, direccion)
 	fmt.Println("Secuencia final rotada =", listaFinal)
 
+	//Ejercicio Zapatos
+	agregarZapato("Nike", 50000, 42, 5)
+	agregarZapato("Adidas", 60000, 40, 3)
+	agregarZapato("Puma", 45000, 38, 7)
+
+	// Realizar ventas de zapatos
+	venderZapato("Nike", 42)
+	venderZapato("Adidas", 40)
+	venderZapato("Puma", 38)
+	venderZapato("Nike", 42)
+	venderZapato("Nike", 42)
+	venderZapato("Puma", 38)
+	venderZapato("Reebok", 39) // Zapato no existente en inventario
+	venderZapato("Adidas", 40) // Agotado el stock de Adidas talla 40
+
+	// Imprimir inventario restante
+	fmt.Println("\nInventario restante:")
+	for _, zapato := range inventario {
+		fmt.Printf("Modelo: %s, Talla: %d, Stock: %d\n", zapato.modelo, zapato.talla, zapato.stock)
+	}
 }
