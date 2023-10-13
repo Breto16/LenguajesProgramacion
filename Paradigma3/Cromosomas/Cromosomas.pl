279a430b360e1fb358a319edfd76bde51981dc65
Breto16 <50472204+Breto16@users.noreply.github.com>
2023-10-13 12:28:40 -0600

    Update Cromosomas.pl

--- Paradigma3/Cromosomas/Cromosomas.pl
+++ Paradigma3/Cromosomas/Cromosomas.pl
@@ -1,11 +1,3 @@
-% Predicado que calcula el porcentaje de exactitud entre dos cromosomas.
-porcentaje_exactitud([], [], 100).
-porcentaje_exactitud([X|Sujeto], [X|Candidato], Porcentaje) :-
-    porcentaje_exactitud(Sujeto, Candidato, PorcentajeRestante),
-    Porcentaje is PorcentajeRestante + 10.
-porcentaje_exactitud([_|Sujeto], [_|Candidato], Porcentaje) :-
-    porcentaje_exactitud(Sujeto, Candidato, Porcentaje).
-
 % Base de Datos de Personas (nombre, cromosoma).
 persona('Persona1', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).
 persona('Persona2', [1, 2, 3, 4, 5, 11, 7, 8, 9, 10]).
@@ -19,13 +11,17 @@ persona('Persona9', [7, 14, 21, 28, 35, 42, 49, 56, 63, 70]).
 persona('Persona10', [8, 16, 24, 32, 40, 48, 56, 64, 72, 80]).
 
 % Predicado para verificar si la persona tiene la mayor exactitud.
-mayor_exactitud(Persona, Cromosoma) :- 
-    persona(Persona, CromosomaPersona),
+mayor_exactitud(Nombre, Cromosoma) :-
+    persona(Nombre, CromosomaPersona),
     porcentaje_exactitud(CromosomaPersona, Cromosoma, Porcentaje),
-    forall((persona(OtraPersona, OtroCromosoma), OtraPersona \= Persona),
+    forall((persona(OtraNombre, OtroCromosoma), OtraNombre \= Nombre),
            (porcentaje_exactitud(OtroCromosoma, Cromosoma, OtroPorcentaje),
             Porcentaje >= OtroPorcentaje)).
 
-% Consulta de ejemplo.
-% ¿'Persona3' tiene el cromosoma más similar a [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]?
-?- mayor_exactitud('Persona3', [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]).
+% Predicado que calcula el porcentaje de exactitud entre dos cromosomas.
+porcentaje_exactitud([], [], 100).
+porcentaje_exactitud([X|Sujeto], [X|Candidato], Porcentaje) :-
+    porcentaje_exactitud(Sujeto, Candidato, PorcentajeRestante),
+    Porcentaje is PorcentajeRestante + 10.
+porcentaje_exactitud([_|Sujeto], [_|Candidato], Porcentaje) :-
+    porcentaje_exactitud(Sujeto, Candidato, Porcentaje).
